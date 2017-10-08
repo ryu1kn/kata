2b77583b7aed146e0d52443b86c3669b0ccc980e
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-10-08 13:19:09 +1100

    Bowling kata with scala

--- scala/src/main/scala/example/Game.scala
+++ scala/src/main/scala/example/Game.scala
@@ -1,9 +1,17 @@
 package example
 
-class Game {
-  def score() = 0
+object Game {
 
-  def roll(point: Int) = {
+  def score(rolls: List[Int]): Int = rolls match {
+    case r1 :: r2 :: r3 :: tail if isStrike(r1) => 10 + r2 + r3 + score(r2 :: r3 :: tail)
+    case r1 :: r2 :: r3 :: tail if isSpare(r1, r2) => 10 + r3 + score(r3 :: tail)
+    case r :: tail => r + score(tail)
+    case _ => 0
   }
+
+  private def isStrike(roll: Int) = roll == 10
+
+
+  private def isSpare(roll1: Int, roll2: Int) = roll1 + roll2 == 10
 
 }
--- scala/src/test/scala/example/GameSpec.scala
+++ scala/src/test/scala/example/GameSpec.scala
@@ -4,12 +4,29 @@ import org.scalatest._
 
 class GameSpec extends FlatSpec with Matchers {
 
-  "Player" should "play a gutter game" in {
-    val g = new Game()
-    for (i <- 0 to 20) {
-      g.roll(0)
-    }
-    g.score() shouldEqual 0
+  "Game" should "calculate a score for a gutter game" in {
+    val rolls = List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    Game.score(rolls) shouldEqual 0
+  }
+
+  "Game" should "calculate a score for all 1 point game" in {
+    val rolls = List(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+    Game.score(rolls) shouldEqual 20
+  }
+
+  "Game" should "calculate a score for one spare game" in {
+    val rolls = List(4, 6, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    Game.score(rolls) shouldEqual 16
+  }
+
+  "Game" should "calculate a score for one strike game" in {
+    val rolls = List(10, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+    Game.score(rolls) shouldEqual 24
+  }
+
+  "Game" should "calculate a score for the 3rd throw in the tenth frame" in {
+    val rolls = List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 7, 10)
+    Game.score(rolls) shouldEqual 30
   }
 
 }
