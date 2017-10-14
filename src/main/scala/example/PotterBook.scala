66e3197c4944024a28c5e1616250904a065e841f
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-10-14 19:23:03 +1100

    Define Book type as an alias of Int to better communicate the intention

--- src/main/scala/example/PotterBook.scala
+++ src/main/scala/example/PotterBook.scala
@@ -2,22 +2,24 @@ package example
 
 object PotterBook {
 
+  type Book = Int
+
   val UNIT_PRICE = 8
   val DISTINCT_BOOK_DISCOUNTS = List(0, 0.05, 0.1, 0.2, 0.25)
 
-  def price(bookIds: List[Int]): Double = {
+  def price(bookIds: List[Book]): Double = {
     if (bookIds.size == 0) return 0
 
     val (uniqueIds, remainings) = findUniqueBooks(bookIds)
     subsetPrice(uniqueIds) + price(remainings)
   }
 
-  private def subsetPrice(distinctBooks: List[Int]) = {
+  private def subsetPrice(distinctBooks: List[Book]) = {
     val discountRate = DISTINCT_BOOK_DISCOUNTS(distinctBooks.size - 1)
     UNIT_PRICE * (1 - discountRate) * distinctBooks.size
   }
 
-  private def findUniqueBooks(bookIds: List[Int]): (List[Int], List[Int]) = bookIds match {
+  private def findUniqueBooks(bookIds: List[Book]): (List[Book], List[Book]) = bookIds match {
     case List(b) => (List(b), List())
     case b :: tail =>
       val (uniqueIds, remainings) = findUniqueBooks(tail)
