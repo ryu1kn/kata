f77b5c11119e402b0604c88f25dae7026241a706
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-10-19 22:40:51 +1100

    Rewrite custom recursive function with fold

--- src/main/scala/example/PotterBook.scala
+++ src/main/scala/example/PotterBook.scala
@@ -7,11 +7,11 @@ object PotterBook {
   val UNIT_PRICE = 8
   val DISTINCT_BOOK_DISCOUNTS = List(0, 0.05, 0.1, 0.2, 0.25)
 
-  def price(bookIds: List[Book]): Double = {
-    if (bookIds.size == 0) return 0
+  def price(books: List[Book]): Double = {
+    if (books.size == 0) return 0
 
-    val (uniqueIds, remainings) = findUniqueBooks(bookIds)
-    subsetPrice(uniqueIds.size) + price(remainings)
+    val (uniqueBooks, remainingBooks) = findUniqueBooks(books)
+    subsetPrice(uniqueBooks.size) + price(remainingBooks)
   }
 
   private def subsetPrice(distinctBookCount: Int): Double = {
@@ -19,12 +19,11 @@ object PotterBook {
     UNIT_PRICE * (1 - discountRate) * distinctBookCount
   }
 
-  private def findUniqueBooks(bookIds: List[Book]): (List[Book], List[Book]) = bookIds match {
-    case List(b) => (List(b), List())
-    case b :: tail =>
-      val (uniqueIds, remainings) = findUniqueBooks(tail)
-      if (uniqueIds.contains(b)) (uniqueIds, b :: remainings)
-      else (b :: uniqueIds, remainings)
-  }
+  private def findUniqueBooks(books: List[Book]): (List[Book], List[Book]) =
+    books.foldLeft((List[Book](), List[Book]())) {
+      case ((uniqueBooks, remainingBooks), book) =>
+        if (uniqueBooks.contains(book)) (uniqueBooks, book :: remainingBooks)
+        else (book :: uniqueBooks, remainingBooks)
+    }
 
 }
