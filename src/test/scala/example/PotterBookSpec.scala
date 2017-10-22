28c58dd6bf7c9f1936fa73281932ff4f8d34cc4f
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-10-22 23:10:09 +1100

    Change indentation

--- src/test/scala/example/PotterBookSpec.scala
+++ src/test/scala/example/PotterBookSpec.scala
@@ -66,4 +66,3 @@ class PotterBookSpec extends FlatSpec with Matchers {
   }
 
 }
-
--- src/test/scala/example/SampleSpec.scala
+++ src/test/scala/example/SampleSpec.scala
@@ -32,11 +32,12 @@ class SampleSpec extends FlatSpec with Matchers {
 
   "Edge cases" should "be considered" in {
     price(List(0, 0, 1, 1, 2, 2, 3, 4)) shouldEqual 2 * (8 * 4 * 0.8)
-    price(List(0, 0, 0, 0, 0,
-        1, 1, 1, 1, 1,
-        2, 2, 2, 2,
-        3, 3, 3, 3, 3,
-        4, 4, 4, 4)) shouldEqual 3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8)
+    price(List(
+      0, 0, 0, 0, 0,
+      1, 1, 1, 1, 1,
+      2, 2, 2, 2,
+      3, 3, 3, 3, 3,
+      4, 4, 4, 4)) shouldEqual 3 * (8 * 5 * 0.75) + 2 * (8 * 4 * 0.8)
   }
 
 }
