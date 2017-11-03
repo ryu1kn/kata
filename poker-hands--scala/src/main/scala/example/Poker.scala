5c49a272d959bb22fce63a46894cb969fc2f7a8c
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-11-03 22:19:07 +1100

    Captured the concept of hand as a class

--- poker-hands--scala/src/main/scala/example/Poker.scala
+++ poker-hands--scala/src/main/scala/example/Poker.scala
@@ -29,60 +29,70 @@ object Poker {
     }
   }
 
-  def play(hand1: String, hand2: String): String = {
-    val cards = hand1.split(' ').toList.map {
-      case Card(card) => card
+  case class Hand(cards: List[Card])
+
+  object Hand {
+
+    def create(hand: String): Hand = {
+      val cards = hand.split(' ').toList.map { case Card(card) => card }
+      Hand(cards)
     }
-    if (isStraightFlush(cards)) buildMessage("straight flush", cards.last)
-    else if (isFourOfAKind(cards)) buildMessage("four of a kind", findMostCommonNumberCard(cards))
-    else if (isFullHouse(cards)) buildMessage("full house", findMostCommonNumberCard(cards))
-    else if (isFlush(cards)) buildMessage("flush", cards.last)
-    else if (isStraight(cards)) buildMessage("straight", cards.last)
-    else if (isThreeOfAKind(cards)) buildMessage("three of a kind", findMostCommonNumberCard(cards))
-    else buildMessage("two pairs", findMostCommonNumberCard(cards))
-  }
 
-  private def isStraightFlush(cards: List[Card]): Boolean = satisfiesRelation(cards, isSameSuiteIncrement)
+    def isStraightFlush(cards: List[Card]): Boolean = satisfiesRelation(cards, isSameSuiteIncrement)
 
-  private def isSameSuiteIncrement(card1: Card, card2: Card): Boolean =
-    card2.suite == card1.suite && card2.intValue == card1.intValue + 1
+    def isSameSuiteIncrement(card1: Card, card2: Card): Boolean =
+      card2.suite == card1.suite && card2.intValue == card1.intValue + 1
 
-  private def isFourOfAKind(cards: List[Card]): Boolean = isNOfAKind(cards, 4)
+    def isFourOfAKind(cards: List[Card]): Boolean = isNOfAKind(cards, 4)
 
-  private def findMostCommonNumberCard(cards: List[Card]): Card = {
-    val (_, mostCommonNumberCards) = cards
-      .groupBy[Int](card => card.intValue)
-      .maxBy { case (_, cardList) => cardList.size }
-    mostCommonNumberCards(0)
-  }
+    def findMostCommonNumberCard(cards: List[Card]): Card = {
+      val (_, mostCommonNumberCards) = cards
+        .groupBy[Int](card => card.intValue)
+        .maxBy { case (_, cardList) => cardList.size }
+      mostCommonNumberCards(0)
+    }
 
-  private def isFullHouse(cards: List[Card]): Boolean =
-    cards
-      .groupBy[Int](card => card.intValue)
-      .map { case (_, cardList) => cardList.size }
-      .toList
-      .sorted match {
-        case List(2, 3) => true
-        case _ => false
+    def isFullHouse(cards: List[Card]): Boolean =
+      cards
+        .groupBy[Int](card => card.intValue)
+        .map { case (_, cardList) => cardList.size }
+        .toList
+        .sorted match {
+          case List(2, 3) => true
+          case _ => false
+        }
+
+    def isFlush(cards: List[Card]): Boolean = cards.groupBy[Char](card => card.suite).size == 1
+
+    def isStraight(cards: List[Card]): Boolean =
+      satisfiesRelation[Int](cards.map(_.intValue).sorted, (card1, card2) => card2 == card1 + 1)
+
+    def isThreeOfAKind(cards: List[Card]): Boolean = isNOfAKind(cards, 3)
+
+    private def isNOfAKind(cards: List[Card], n: Int): Boolean =
+      cards
+        .groupBy[Int](card => card.intValue)
+        .exists { case (_, cardList) => cardList.size == n }
+
+    private def satisfiesRelation[A](list: List[A], f: (A, A) => Boolean): Boolean =
+      list.zipWithIndex.forall {
+        case (_, 0) => true
+        case (el, index) => f(list(index - 1), el)
       }
 
-  private def isFlush(cards: List[Card]): Boolean = cards.groupBy[Char](card => card.suite).size == 1
+  }
 
-  private def isStraight(cards: List[Card]): Boolean =
-    satisfiesRelation[Int](cards.map(_.intValue).sorted, (card1, card2) => card2 == card1 + 1)
-
-  private def isThreeOfAKind(cards: List[Card]): Boolean = isNOfAKind(cards, 3)
-
-  private def isNOfAKind(cards: List[Card], n: Int): Boolean =
-    cards
-      .groupBy[Int](card => card.intValue)
-      .exists { case (_, cardList) => cardList.size == n }
-
-  private def satisfiesRelation[A](list: List[A], f: (A, A) => Boolean): Boolean =
-    list.zipWithIndex.forall {
-      case (el, 0) => true
-      case (el, index) => f(list(index - 1), el)
-    }
+  def play(hand1: String, hand2: String): String = {
+    val hand = Hand.create(hand1)
+    val cards = hand.cards
+    if (Hand.isStraightFlush(cards)) buildMessage("straight flush", cards.last)
+    else if (Hand.isFourOfAKind(cards)) buildMessage("four of a kind", Hand.findMostCommonNumberCard(cards))
+    else if (Hand.isFullHouse(cards)) buildMessage("full house", Hand.findMostCommonNumberCard(cards))
+    else if (Hand.isFlush(cards)) buildMessage("flush", cards.last)
+    else if (Hand.isStraight(cards)) buildMessage("straight", cards.last)
+    else if (Hand.isThreeOfAKind(cards)) buildMessage("three of a kind", Hand.findMostCommonNumberCard(cards))
+    else buildMessage("two pairs", Hand.findMostCommonNumberCard(cards))
+  }
 
   private def buildMessage(handName: String, card: Card): String =
     s"Black wins. - with $handName: ${card.valueName}"
