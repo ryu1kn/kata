84c46d7fb403f33d69e052c959925c0781a9a5d4
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-11-05 22:39:27 +1100

    Refactoring

--- poker-hands--scala/src/main/scala/example/Hand.scala
+++ poker-hands--scala/src/main/scala/example/Hand.scala
@@ -1,78 +1,132 @@
 package example
 
-case class Hand(cards: List[Card])
+import example.Hand._
+
+case class Hand(rank: HandRank, cards: List[Card]) extends Ordered[Hand] {
+  private val STRONGER_HAND = 1
+  private val WEAKER_HAND = -1 * STRONGER_HAND
+
+  override def compare(that: Hand): Int = (this, that) match {
+    case (Hand(StraightFlush, _), Hand(StraightFlush, _)) => StraightFlush.compare(this, that)
+    case (Hand(StraightFlush, _), _) => STRONGER_HAND
+    case (_, Hand(StraightFlush, _)) => WEAKER_HAND
+
+    case (Hand(FourOfAKind, _), _) => STRONGER_HAND
+    case (Hand(FullHouse, _), _) => STRONGER_HAND
+    case (Hand(Flush, _), _) => STRONGER_HAND
+    case (Hand(Straight, _), _) => STRONGER_HAND
+    case (Hand(ThreeOfAKind, _), _) => STRONGER_HAND
+    case (Hand(TwoPairs, _), _) => STRONGER_HAND
+    case (Hand(Pair, _), _) => STRONGER_HAND
+    case (Hand(HighCard, _), _) => STRONGER_HAND
+  }
+}
 
 object Hand {
 
-  trait HandRank {
-    def unapply(hand: Hand): Option[Hand] = hand match {
-      case h if isOfRank(h) => Some(h)
-      case _ => None
-    }
+  val ranks: List[HandRank] = List(StraightFlush, FourOfAKind, FullHouse, Flush, Straight, ThreeOfAKind, TwoPairs, Pair, HighCard)
 
-    protected def isOfRank(hand: Hand): Boolean
+  def create(cardsStr: String): Hand = {
+    val cards = cardsStr.split(" ").toList.map { case Card(card) => card }
+    ranks.find(_.isOfRank(cards)) match {
+      case Some(rank) => Hand(rank, cards)
+    }
   }
 
-  object StraightFlush extends HandRank {
-    override def isOfRank(hand: Hand): Boolean =
-      satisfiesRelation(hand.cards, isSameSuiteIncrement)
+  trait HandRank {
+    def unapply(cards: List[Card]): Option[Hand] = if (isOfRank(cards)) Some(Hand(this, cards)) else None
+    def isOfRank(cards: List[Card]): Boolean
+    val name: String
+    def decider(hand: Hand): Card
+  }
+
+  case object StraightFlush extends HandRank {
+    override val name: String = "straight flush"
+
+    override def isOfRank(cards: List[Card]): Boolean =
+      satisfiesRelation(cards, isSameSuiteIncrement)
 
     private def isSameSuiteIncrement(card1: Card, card2: Card): Boolean =
       card2.suite == card1.suite && card2.intValue == card1.intValue + 1
 
     def compare(hand1: Hand, hand2: Hand): Int = lastCard(hand1).compare(lastCard(hand2))
+
+    override def decider(hand: Hand): Card = lastCard(hand)
   }
 
-  object FourOfAKind extends HandRank {
-    override def isOfRank(hand: Hand): Boolean = isNOfAKind(hand, 4)
+  case object FourOfAKind extends HandRank {
+    override val name: String = "four of a kind"
+
+    override def isOfRank(cards: List[Card]): Boolean = isNOfAKind(cards, 4)
+    override def decider(hand: Hand): Card = findMostCommonNumberCard(hand.cards)
   }
 
-  object FullHouse extends HandRank {
-    override def isOfRank(hand: Hand): Boolean = hasSameNumberGroups(hand, List(2, 3))
+  case object FullHouse extends HandRank {
+    override val name: String = "full house"
+
+    override def isOfRank(cards: List[Card]): Boolean = hasSameNumberGroups(cards, List(2, 3))
+    override def decider(hand: Hand): Card = findMostCommonNumberCard(hand.cards)
   }
 
-  object Flush extends HandRank {
-    override def isOfRank(hand: Hand): Boolean = hand.cards.groupBy[Char](card => card.suite).size == 1
+  case object Flush extends HandRank {
+    override val name: String = "flush"
+
+    override def isOfRank(cards: List[Card]): Boolean = cards.groupBy[Char](card => card.suite).size == 1
+    override def decider(hand: Hand): Card = lastCard(hand)
   }
 
-  object Straight extends HandRank {
-    override def isOfRank(hand: Hand): Boolean =
-      satisfiesRelation[Int](hand.cards.map(_.intValue).sorted, (card1, card2) => card2 == card1 + 1)
+  case object Straight extends HandRank {
+    override val name: String = "straight"
+
+    override def isOfRank(cards: List[Card]): Boolean =
+      satisfiesRelation[Int](cards.map(_.intValue).sorted, (card1, card2) => card2 == card1 + 1)
+    override def decider(hand: Hand): Card = lastCard(hand)
   }
 
-  object ThreeOfAKind extends HandRank {
-    override def isOfRank(hand: Hand): Boolean = isNOfAKind(hand, 3)
+  case object ThreeOfAKind extends HandRank {
+    override val name: String = "three of a kind"
+
+    override def isOfRank(cards: List[Card]): Boolean = isNOfAKind(cards, 3)
+    override def decider(hand: Hand): Card = findMostCommonNumberCard(hand.cards)
   }
 
-  object TwoPairs extends HandRank {
-    override def isOfRank(hand: Hand): Boolean = hasSameNumberGroups(hand, List(1, 2, 2))
+  case object TwoPairs extends HandRank {
+    override val name: String = "two pairs"
+
+    override def isOfRank(cards: List[Card]): Boolean = hasSameNumberGroups(cards, List(1, 2, 2))
+    override def decider(hand: Hand): Card = findMostCommonNumberCard(hand.cards)
   }
 
-  object Pair extends HandRank {
-    override def isOfRank(hand: Hand): Boolean = hasSameNumberGroups(hand, List(1, 1, 1, 2))
+  case object Pair extends HandRank {
+    override val name: String = "pair"
+
+    override def isOfRank(cards: List[Card]): Boolean = hasSameNumberGroups(cards, List(1, 1, 1, 2))
+    override def decider(hand: Hand): Card = findMostCommonNumberCard(hand.cards)
   }
 
-  def create(hand: String): Hand = {
-    val cards = hand.split(' ').toList.map { case Card(card) => card }
-    Hand(cards)
+  case object HighCard extends HandRank {
+    override val name: String = "high card"
+
+    override def isOfRank(cards: List[Card]): Boolean = true
+    override def decider(hand: Hand): Card = lastCard(hand)
   }
 
   def lastCard(hand: Hand): Card = hand.cards.last
 
-  def findMostCommonNumberCard(hand: Hand): Card = {
-    val (_, mostCommonNumberCards) = hand.cards
+  def findMostCommonNumberCard(cards: List[Card]): Card = {
+    val (_, mostCommonNumberCards) = cards
       .groupBy[Int](card => card.intValue)
       .maxBy { case (_, cardList) => cardList.size }
     mostCommonNumberCards.head
   }
 
-  private def isNOfAKind(hand: Hand, n: Int): Boolean =
-    hand.cards
+  private def isNOfAKind(cards: List[Card], n: Int): Boolean =
+    cards
       .groupBy[Int](card => card.intValue)
       .exists { case (_, cardList) => cardList.size == n }
 
-  private def hasSameNumberGroups(hand: Hand, groupSizes: List[Int]): Boolean =
-    hand.cards
+  private def hasSameNumberGroups(cards: List[Card], groupSizes: List[Int]): Boolean =
+    cards
       .groupBy[Int](card => card.intValue)
       .map { case (_, cardList) => cardList.size }
       .toList
--- poker-hands--scala/src/main/scala/example/Poker.scala
+++ poker-hands--scala/src/main/scala/example/Poker.scala
@@ -2,26 +2,27 @@ package example
 
 object Poker {
 
-  import Hand._
+  case class Player(name: String, hand: Hand)
+
+  object Player {
+    def highScorer(player1: Player, player2: Player): Option[Player] = {
+      val comparisonResult = player1.hand.compare(player2.hand)
+      comparisonResult match {
+        case n if n > 0 => Some(player1)
+        case n if n < 0 => Some(player2)
+        case _ => None
+      }
+    }
+  }
 
   def play(hand1: String, hand2: String): String = {
-    (create(hand1), create(hand2)) match {
-      case (StraightFlush(hand1), StraightFlush(hand2)) =>
-        StraightFlush.compare(hand1, hand2) match {
-          case n if n > 0 => buildMessage("Black", "straight flush", lastCard(hand1))
-          case n if n < 0 => buildMessage("White", "straight flush", lastCard(hand2))
-          case _ => "Tie"
-        }
-      case (StraightFlush(hand), _) => buildMessage("Black", "straight flush", lastCard(hand))
-      case (_, StraightFlush(hand)) => buildMessage("White", "straight flush", lastCard(hand))
-      case (FourOfAKind(hand), _) => buildMessage("Black", "four of a kind", findMostCommonNumberCard(hand))
-      case (FullHouse(hand), _) => buildMessage("Black", "full house", findMostCommonNumberCard(hand))
-      case (Flush(hand), _) => buildMessage("Black", "flush", lastCard(hand))
-      case (Straight(hand), _) => buildMessage("Black", "straight", lastCard(hand))
-      case (ThreeOfAKind(hand), _) => buildMessage("Black", "three of a kind", findMostCommonNumberCard(hand))
-      case (TwoPairs(hand), _) => buildMessage("Black", "two pairs", findMostCommonNumberCard(hand))
-      case (Pair(hand), _) => buildMessage("Black", "pair", findMostCommonNumberCard(hand))
-      case (hand, _) => buildMessage("Black", "high card", lastCard(hand))
+    val player1 = Player("Black", Hand.create(hand1))
+    val player2 = Player("White", Hand.create(hand2))
+    val winner = Player.highScorer(player1, player2)
+
+    winner match {
+      case Some(Player(name, hand)) => buildMessage(name, hand.rank.name, hand.rank.decider(hand))
+      case None => "Tie"
     }
   }
 
