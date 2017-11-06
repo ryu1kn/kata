501cce6baf62e8a9a3f22280fe472f7a3da46d74
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-11-06 15:29:35 +1100

    Minor refactoring

--- poker-hands--scala/src/main/scala/example/Poker.scala
+++ poker-hands--scala/src/main/scala/example/Poker.scala
@@ -5,22 +5,19 @@ object Poker {
   case class Player(name: String, hand: Hand)
 
   object Player {
-    def highScorer(player1: Player, player2: Player): Option[Player] = {
-      val comparisonResult = player1.hand.compare(player2.hand)
-      comparisonResult match {
+    def getWinner(player1: Player, player2: Player): Option[Player] =
+      player1.hand.compare(player2.hand) match {
         case n if n > 0 => Some(player1)
         case n if n < 0 => Some(player2)
         case _ => None
       }
-    }
   }
 
   def play(hand1: String, hand2: String): String = {
     val player1 = Player("Black", Hand.create(hand1))
     val player2 = Player("White", Hand.create(hand2))
-    val winner = Player.highScorer(player1, player2)
 
-    winner match {
+    Player.getWinner(player1, player2) match {
       case Some(Player(name, hand)) => buildMessage(name, hand.rank.name, hand.rank.decider(hand))
       case None => "Tie"
     }
