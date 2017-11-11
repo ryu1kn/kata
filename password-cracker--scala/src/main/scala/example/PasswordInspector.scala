80b0f1090ba071d72d55ce0934c8b7c8bb99a008
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-11-11 17:11:57 +1100

    Deleted redundant logics

--- password-cracker--scala/src/main/scala/example/PasswordInspector.scala
+++ password-cracker--scala/src/main/scala/example/PasswordInspector.scala
@@ -2,35 +2,31 @@ package example
 
 object PasswordInspector {
 
-  def decompose(attempt: String, passwords: List[String]): Option[List[String]] = {
-    val exactMatch = passwords.find(_ == attempt)
-    exactMatch match {
-      case Some(m) => Some(List(m))
-      case _ =>
-        if (attempt == passwords.mkString) Some(passwords)
-        else if (attempt == passwords.reverse.mkString) Some(passwords.reverse)
-        else findFullGrownBranch(attempt, gradualDecompose(attempt, passwords))
-    }
+  private lazy val TREE_ROOT_WORD = ""
+
+  def decompose(attempt: String, passwords: List[String]): Option[List[String]] =
+    findFullGrownBranch(attempt, buildWordTree(attempt, passwords))
+
+  private case class WordTree(prefix: String, rest: List[WordTree])
+
+  private def buildWordTree(attempt: String, passwords: List[String]): WordTree = {
+    def buildWordTree_(attempt: String): List[WordTree] =
+      passwords
+        .filter(attempt.startsWith)
+        .map(password => WordTree(password, buildWordTree_(attempt.stripPrefix(password))))
+
+    WordTree(TREE_ROOT_WORD, buildWordTree_(attempt))
   }
 
-  private case class Decomp(prefix: String, rest: List[Decomp])
-
-  private def gradualDecompose(attempt: String, passwords: List[String]): Decomp = {
-    def gradualDecompose_(attempt: String): List[Decomp] =
-      passwords.filter(attempt.startsWith)
-        .map(password => Decomp(password, gradualDecompose_(attempt.stripPrefix(password))))
-    Decomp("", gradualDecompose_(attempt))
-  }
-
-  private def findFullGrownBranch(attempt: String, node: Decomp): Option[List[String]] = {
-    def reconstructAttempts(decomp: Decomp): List[List[String]] = decomp match {
-      case Decomp(password, List()) => List(List(password))
-      case Decomp(password, decomps) => decomps.flatMap(reconstructAttempts).map(password :: _)
+  private def findFullGrownBranch(attempt: String, tree: WordTree): Option[List[String]] = {
+    def flattenWordTree(tree: WordTree): List[List[String]] = tree match {
+      case WordTree(password, List()) => List(List(password))
+      case WordTree(password, subTree) => subTree.flatMap(flattenWordTree).map(password :: _)
     }
-    reconstructAttempts(node).find(_.mkString == attempt) match {
-      case Some("" :: rest) => Some(rest)
-      case _ => None
-    }
+
+    flattenWordTree(tree)
+      .map { case TREE_ROOT_WORD :: tail => tail }
+      .find(_.mkString == attempt)
   }
 
 }
