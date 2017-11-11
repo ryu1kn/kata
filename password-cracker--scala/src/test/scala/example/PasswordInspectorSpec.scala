304057350d6a9866b8259dcfe02ac17eb1199891
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-11-11 17:22:08 +1100

    Confirm that it passes all sample tests

--- password-cracker--scala/src/test/scala/example/PasswordInspectorSpec.scala
+++ password-cracker--scala/src/test/scala/example/PasswordInspectorSpec.scala
@@ -30,4 +30,35 @@ class PasswordInspectorSpec extends FlatSpec with Matchers {
     decompose("attempt", List("at", "pt", "tem")) shouldEqual Some(List("at", "tem", "pt"))
   }
 
+  it should "use the same password multiple times if necessary" in {
+    decompose("pass", List("pa", "s")) shouldEqual Some(List("pa", "s", "s"))
+  }
+
+  it should "pass sample test #1" in {
+    val passwords = List("because", "can", "do", "must", "we", "what")
+    val expected = List("we", "do", "what", "we", "must", "because", "we", "can")
+    decompose("wedowhatwemustbecausewecan", passwords) shouldEqual Some(expected)
+  }
+
+  it should "pass sample test #2" in {
+    decompose("helloworld", List("hello", "planet")) shouldEqual None
+  }
+
+  it should "pass sample test #3" in {
+    decompose("abcd", List("ab", "abcd", "cd")) shouldEqual Some(List("ab", "cd"))
+  }
+
+  it should "pass sample test #4" in {
+    decompose("zfzahm", List("ozkxyhkcst", "xvglh", "hpdnb", "zfzahm")) shouldEqual Some(List("zfzahm"))
+  }
+
+  it should "pass sample test #5" in {
+    decompose("gurwgrb", List("gurwgrb", "maqz", "holpkhqx", "aowypvopum")) shouldEqual Some(List("gurwgrb"))
+  }
+
+  it should "pass sample test #6" in {
+    val passwords = List("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa")
+    decompose("aaaaaaaaaab", passwords) shouldEqual None
+  }
+
 }
