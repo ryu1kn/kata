7881152a741c3b4f4e080caa22f15f77e4f51d01
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-11-12 21:03:00 +1100

    Password Cracker solution with Haskell

--- password-cracker--haskell/src/Lib.hs
+++ password-cracker--haskell/src/Lib.hs
@@ -2,5 +2,25 @@ module Lib
     ( decompose
     ) where
 
+import Data.List
+import Data.Foldable (concatMap)
+
+data WordTree = WordTree String [WordTree]
+
 decompose :: String -> [String] -> Maybe [String]
-decompose attempt [_] = Just [attempt]
+decompose attempt passwords = findFullGrownBranch attempt (buildWordTree attempt passwords)
+
+buildWordTree :: String -> [String] -> WordTree
+buildWordTree attempt passwords = WordTree "" (buildWordTree_ attempt passwords)
+    where
+        buildWordTree_ attempt passwords =
+            let filtered = filter (\x -> isPrefixOf x attempt) passwords
+            in map (\x -> WordTree x (buildWordTree_ (drop (length x) attempt) passwords)) filtered
+
+findFullGrownBranch :: String -> WordTree -> Maybe [String]
+findFullGrownBranch attempt tree =
+    let noRootNode = map (drop 1) $ flattenWordTree tree
+    in find (\x -> attempt == intercalate "" x) noRootNode
+    where
+        flattenWordTree (WordTree password []) = [[password]]
+        flattenWordTree (WordTree password subTree) = map ((:) password) (concatMap flattenWordTree subTree)
--- password-cracker--haskell/test/Spec.hs
+++ password-cracker--haskell/test/Spec.hs
@@ -9,3 +9,40 @@ spec = describe "decompose" $ do
 
     it "tells a password itself should be accepted" $
         decompose "password" ["password"] `shouldBe` Just ["password"]
+
+    it "uses the 2nd password to build login attempt string" $
+        decompose "password2" ["password1", "password2"] `shouldBe` Just ["password2"]
+
+    it "tells if login attempt cannot be built from the passwords" $
+        decompose "attempt" ["password"] `shouldBe` Nothing
+
+    it "tells if login attempt string can be built from 2 passwords" $
+        decompose "attempt" ["att", "empt"] `shouldBe` Just ["att", "empt"]
+
+    it "works even if 2 passwords are given in reverse order" $
+        decompose "attempt" ["empt", "att"] `shouldBe` Just ["att", "empt"]
+
+    it "works even if 3 passwords are given in neither correct nor reverse order" $
+        decompose "attempt" ["at", "pt", "tem"] `shouldBe` Just ["at", "tem", "pt"]
+
+    it "uses the same password multiple times if necessary" $
+        decompose "pass" ["pa", "s"] `shouldBe` Just ["pa", "s", "s"]
+
+    it "passes sample test #1" $
+        decompose "wedowhatwemustbecausewecan" ["because", "can", "do", "must", "we", "what"]
+            `shouldBe` Just ["we", "do", "what", "we", "must", "because", "we", "can"]
+
+    it "passes sample test #2" $
+        decompose "helloworld" ["hello", "planet"] `shouldBe` Nothing
+
+    it "passes sample test #3" $
+        decompose "abcd" ["ab", "abcd", "cd"] `shouldBe` Just ["ab", "cd"]
+
+    it "passes sample test #4" $
+        decompose "zfzahm" ["ozkxyhkcst", "xvglh", "hpdnb", "zfzahm"] `shouldBe` Just ["zfzahm"]
+
+    it "passes sample test #5" $
+        decompose "gurwgrb" ["gurwgrb", "maqz", "holpkhqx", "aowypvopum"] `shouldBe` Just ["gurwgrb"]
+
+    it "passes sample test #6" $
+        decompose "aaaaaaaaaab" ["a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa"] `shouldBe` Nothing
