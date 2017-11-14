cd8cb9017a649e94482c9cea19455a17cb5d01c0
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-11-14 23:53:22 +1100

    Support numbers 1 - 8

--- roman-numeral--haskell/src/Lib.hs
+++ roman-numeral--haskell/src/Lib.hs
@@ -5,4 +5,10 @@ module Lib
 import Data.Char
 
 encode :: Int -> String
-encode x = "I"
+encode x
+    | x < 4 = take x $ repeat 'I'
+    | otherwise = aroundV x
+    where
+        aroundV x = let leftI = take (min (5 - x) 1) $ repeat 'I'
+                        rightI = take (max (x - 5) 0) $ repeat 'I'
+                    in leftI ++ "V" ++ rightI
--- roman-numeral--haskell/test/Spec.hs
+++ roman-numeral--haskell/test/Spec.hs
@@ -9,3 +9,21 @@ spec = describe "encode" $ do
 
     it "converts 1 to I" $
         encode 1 `shouldBe` "I"
+
+    it "converts 2 to II" $
+        encode 2 `shouldBe` "II"
+
+    it "converts 4 to IV" $
+        encode 4 `shouldBe` "IV"
+
+    it "converts 5 to V" $
+        encode 5 `shouldBe` "V"
+
+    it "converts 6 to VI" $
+        encode 6 `shouldBe` "VI"
+
+    it "converts 8 to VIII" $
+        encode 8 `shouldBe` "VIII"
+
+    -- it "converts 9 to IX" $
+    --     encode 9 `shouldBe` "IX"
