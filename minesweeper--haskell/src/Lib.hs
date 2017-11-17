703048ca87a2789e0abe0b505b823e4a3a1dea8f
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2017-11-18 01:03:33 +1100

    Formatting haskell code

--- minesweeper--haskell/src/Lib.hs
+++ minesweeper--haskell/src/Lib.hs
@@ -2,26 +2,27 @@ module Lib
     ( fillNumbers
     ) where
 
-import Data.Char
+import           Data.Char
 
 {-# ANN module "HLint: ignore Use String" #-}
 
 fillNumbers :: [[Char]] -> [[Char]]
 fillNumbers board =
-    [
-        [ countSurroundingBombs (i, j) board
-            | j <- [0 .. length (board!!i) - 1] ]
-            | i <- [0 .. length board - 1]
+    [ [ countSurroundingBombs (i, j) board
+      | j <- [0 .. length (board!!i) - 1]
+      ]
+    | i <- [0 .. length board - 1]
     ]
 
 countSurroundingBombs :: (Int, Int) -> [[Char]] -> Char
 countSurroundingBombs (i, j) board
     | board !! i !! j == '*' = '*'
     | otherwise =
-        let subBoard = [ board !! x !! y | x <- [max 0 (i-1)..min (length board - 1) (i+1)],
-                                           y <- [max 0 (j-1)..min (length (board!!x) - 1) (j+1)]
+        let subBoard = [ board !! x !! y
+                       | x <- [max 0 (i-1)..min (length board - 1) (i+1)]
+                       , y <- [max 0 (j-1)..min (length (board!!x) - 1) (j+1)]
                        ]
         in toDigit . countBombs $ subBoard
-    where
-        countBombs = length . filter (== '*')
-        toDigit = chr . (+48)
+  where
+    countBombs = length . filter (== '*')
+    toDigit = chr . (+48)
--- roman-numeral--haskell/src/Lib.hs
+++ roman-numeral--haskell/src/Lib.hs
@@ -2,13 +2,13 @@ module Lib
     ( encode
     ) where
 
-import Data.Char
+import           Data.Char
 
 encode :: Int -> String
 encode x
-    | x < 4 = replicate x 'I'
+    | x < 4     = replicate x 'I'
     | otherwise = aroundV x
-    where
-        aroundV x = let leftI = replicate (min (5 - x) 1) 'I'
-                        rightI = replicate (max (x - 5) 0) 'I'
-                    in leftI ++ "V" ++ rightI
+  where
+    aroundV x = let leftI  = replicate (min (5 - x) 1) 'I'
+                    rightI = replicate (max (x - 5) 0) 'I'
+                in leftI ++ "V" ++ rightI
