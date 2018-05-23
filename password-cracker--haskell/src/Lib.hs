429df94152412fbc241de72afc4f3b875463b863
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-05-23 21:07:49 +1000

    Make WordTree displayable

--- password-cracker--haskell/src/Lib.hs
+++ password-cracker--haskell/src/Lib.hs
@@ -5,20 +5,24 @@ module Lib
 import           Data.Foldable (concatMap)
 import           Data.List
 
-data WordTree = WordTree String [WordTree]
+data WordTree a = WordTree a [WordTree a]
+
+instance Show a => Show (WordTree a) where
+    show (WordTree x ws) = "(" ++ show x ++ ", [" ++ subTrees ++ "])"
+        where subTrees = intercalate "," (map show ws)
 
 decompose :: String -> [String] -> Maybe [String]
 decompose attempt passwords = findFullGrownBranch attempt $ buildWordTree attempt passwords
 
-buildWordTree :: String -> [String] -> WordTree
-buildWordTree attempt passwords = WordTree "" $ buildSubTreeList attempt
+buildWordTree :: String -> [String] -> WordTree String
+buildWordTree attempt passwords = WordTree "" (buildSubTreeList attempt)
     where
         buildSubTreeList attempt =
-            map (buildSubTree attempt) $ filter (`isPrefixOf` attempt) passwords
-        buildSubTree attempt pwd = WordTree pwd $ buildSubTreeList (removePrefix pwd attempt)
-        removePrefix prefix = drop $ length prefix
+            map (buildSubTree attempt) (filter (`isPrefixOf` attempt) passwords)
+        buildSubTree attempt pwd = WordTree pwd (buildSubTreeList $ removePrefix pwd attempt)
+        removePrefix = drop . length
 
-findFullGrownBranch :: String -> WordTree -> Maybe [String]
+findFullGrownBranch :: String -> WordTree String -> Maybe [String]
 findFullGrownBranch attempt tree = find (isSameWhenJoined attempt) $ dropRootNodeFromEach $ flattenWordTree tree
     where
         flattenWordTree (WordTree password []) = [[password]]
