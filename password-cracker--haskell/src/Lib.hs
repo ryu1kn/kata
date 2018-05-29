24d99f6889dbd2be9a99f3c15de1e06354f14aa6
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-05-29 21:03:21 +1000

    Use Maybe instead of List to represent a parser

--- password-cracker--haskell/src/Lib.hs
+++ password-cracker--haskell/src/Lib.hs
@@ -5,45 +5,43 @@ module Lib
 import           Control.Applicative
 import           Data.Char
 
-newtype Parser a = P (String -> [(a,String)])
+newtype Parser a = P (String -> Maybe (a,String))
 
-parse :: Parser a -> String -> [(a,String)]
+parse :: Parser a -> String -> Maybe (a,String)
 parse (P p) inp = p inp
 
 instance Functor Parser where
     -- fmap :: (a -> b) -> Parser a -> Parser b
     fmap g p = P (\inp -> case parse p inp of
-                            []        -> []
-                            [(v,out)] -> [(g v, out)])
+                            Nothing      -> Nothing
+                            Just (v,out) -> Just (g v, out))
 
 instance Applicative Parser where
     -- pure :: a -> Parser a
-    pure v = P (\inp -> [(v,inp)])
+    pure v = P (\inp -> Just (v,inp))
 
     -- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
     pg <*> px = P (\inp -> case parse pg inp of
-                                []        -> []
-                                [(g,out)] -> parse (fmap g px) out)
+                                Nothing      -> Nothing
+                                Just (g,out) -> parse (fmap g px) out)
 
 instance Monad Parser where
     -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
     p >>= f = P (\inp -> case parse p inp of
-                            []        -> []
-                            [(v,out)] -> parse (f v) out)
+                            Nothing      -> Nothing
+                            Just (v,out) -> parse (f v) out)
 
 instance Alternative Parser where
     -- empty :: Parser a
-    empty = P (\_ -> [])
+    empty = P (\_ -> Nothing)
 
     -- (<|>) :: Parser a -> Parser a -> Parser a
-    p <|> q = P (\inp -> case parse p inp of
-                            []        -> parse q inp
-                            [(v,out)] -> [(v,out)])
+    p <|> q = P (\inp -> parse p inp <|> parse q inp)
 
 item :: Parser Char
 item = P (\inp -> case inp of
-                    []     -> []
-                    (x:xs) -> [(x,xs)])
+                    []     -> Nothing
+                    (x:xs) -> Just (x,xs))
 
 sat :: (Char -> Bool) -> Parser Char
 sat p = do x <- item
@@ -62,9 +60,9 @@ password :: [String] -> Parser String
 password ws = foldl1 (<|>) (fmap string ws)
 
 passwords :: [String] -> Parser [String]
-passwords ws = many (password ws)
+passwords ws = some (password ws)
 
 decompose :: String -> [String] -> Maybe [String]
 decompose attempt pwds = case parse (passwords pwds) attempt of
-                            [(pwds,[])] -> Just pwds
-                            _           -> Nothing
+                            Just (ws,[]) -> Just ws
+                            _            -> Nothing
