25556fa54aae46be99eaac2409da3507ce845395
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-05-29 20:00:01 +1000

    Rewrite Password Cracker exercise with Parser

--- password-cracker--haskell/src/Lib.hs
+++ password-cracker--haskell/src/Lib.hs
@@ -2,30 +2,69 @@ module Lib
     ( decompose
     ) where
 
-import           Data.Foldable (concatMap)
-import           Data.List
+import           Control.Applicative
+import           Data.Char
 
-data WordTree a = WordTree a [WordTree a]
+newtype Parser a = P (String -> [(a,String)])
 
-instance Show a => Show (WordTree a) where
-    show (WordTree x ws) = "(" ++ show x ++ ", [" ++ subTrees ++ "])"
-        where subTrees = intercalate "," (map show ws)
+parse :: Parser a -> String -> [(a,String)]
+parse (P p) inp = p inp
+
+instance Functor Parser where
+    -- fmap :: (a -> b) -> Parser a -> Parser b
+    fmap g p = P (\inp -> case parse p inp of
+                            []        -> []
+                            [(v,out)] -> [(g v, out)])
+
+instance Applicative Parser where
+    -- pure :: a -> Parser a
+    pure v = P (\inp -> [(v,inp)])
+
+    -- (<*>) :: Parser (a -> b) -> Parser a -> Parser b
+    pg <*> px = P (\inp -> case parse pg inp of
+                                []        -> []
+                                [(g,out)] -> parse (fmap g px) out)
+
+instance Monad Parser where
+    -- (>>=) :: Parser a -> (a -> Parser b) -> Parser b
+    p >>= f = P (\inp -> case parse p inp of
+                            []        -> []
+                            [(v,out)] -> parse (f v) out)
+
+instance Alternative Parser where
+    -- empty :: Parser a
+    empty = P (\_ -> [])
+
+    -- (<|>) :: Parser a -> Parser a -> Parser a
+    p <|> q = P (\inp -> case parse p inp of
+                            []        -> parse q inp
+                            [(v,out)] -> [(v,out)])
+
+item :: Parser Char
+item = P (\inp -> case inp of
+                    []     -> []
+                    (x:xs) -> [(x,xs)])
+
+sat :: (Char -> Bool) -> Parser Char
+sat p = do x <- item
+           if p x then return x else empty
+
+char :: Char -> Parser Char
+char x = sat (== x)
+
+string :: String -> Parser String
+string [] = return []
+string (x:xs) = do char x
+                   string xs
+                   return (x:xs)
+
+password :: [String] -> Parser String
+password ws = foldl1 (<|>) (fmap string ws)
+
+passwords :: [String] -> Parser [String]
+passwords ws = many (password ws)
 
 decompose :: String -> [String] -> Maybe [String]
-decompose attempt passwords = findFullGrownBranch attempt $ buildWordTree attempt passwords
-
-buildWordTree :: String -> [String] -> WordTree String
-buildWordTree attempt passwords = WordTree "" (buildSubTreeList attempt)
-    where
-        buildSubTreeList attempt =
-            map (buildSubTree attempt) (filter (`isPrefixOf` attempt) passwords)
-        buildSubTree attempt pwd = WordTree pwd (buildSubTreeList $ removePrefix pwd attempt)
-        removePrefix = drop . length
-
-findFullGrownBranch :: String -> WordTree String -> Maybe [String]
-findFullGrownBranch attempt tree = find (isSameWhenJoined attempt) $ dropRootNodeFromEach $ flattenWordTree tree
-    where
-        flattenWordTree (WordTree password []) = [[password]]
-        flattenWordTree (WordTree password subTree) = map ((:) password) (concatMap flattenWordTree subTree)
-        dropRootNodeFromEach = map (drop 1)
-        isSameWhenJoined fullstring strings = fullstring == intercalate "" strings
+decompose attempt pwds = case parse (passwords pwds) attempt of
+                            [(pwds,[])] -> Just pwds
+                            _           -> Nothing
