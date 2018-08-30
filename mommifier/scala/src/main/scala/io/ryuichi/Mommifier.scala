d72b8580ec9996b67238365eea35de63d0929e0a
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-08-30 11:09:27 +1000

    Refactoring

--- mommifier/scala/src/main/scala/io/ryuichi/Mommifier.scala
+++ mommifier/scala/src/main/scala/io/ryuichi/Mommifier.scala
@@ -3,20 +3,25 @@ package io.ryuichi
 object Mommifier {
 
   def mommify(input: String): String =
-    if (input.isEmpty || hasTooLessVowels(input)) input
-    else replaceVowels(input)
+    if (needsMommify(input)) doMommify(input)
+    else input
+
+  private def needsMommify(text: String) =
+    !text.isEmpty && getVowelRatio(text) > 0.3
 
   private def isVowel(c: Char) = "aeiou".contains(c)
 
-  private def hasTooLessVowels(text: String) = getVowelRatio(text) <= 0.3
-
   private def getVowelRatio(text: String) = text.count(isVowel).toDouble / text.length
 
-  private def replaceVowels(text: String): String = text
-      .foldRight(List.empty[Char]) {
-        case (c1, c2 :: rest) if isVowel(c1) && isVowel(c2) => c2 :: rest
-        case (c, list) => c :: list
-      }
-      .flatMap(c => if (isVowel(c)) "mommy" else c.toString).mkString
+  private def doMommify(text: String) = replaceVowels(squashVowels(text))
+
+  private def squashVowels(text: String): String = text
+    .foldRight(List.empty[Char]) {
+      case (c1, c2 :: rest) if isVowel(c1) && isVowel(c2) => c2 :: rest
+      case (c, list) => c :: list
+    }
+    .mkString
+
+  private def replaceVowels(text: String): String = text.flatMap(c => if (isVowel(c)) "mommy" else c.toString)
 
 }
