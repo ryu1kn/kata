851a0f7c2bad0f50c36f05733ec7fe643551bd7e
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2018-09-05 16:57:27 +1000

    Use case object instead of just object

--- family/scala/src/main/scala/io/ryuichi/Person.scala
+++ family/scala/src/main/scala/io/ryuichi/Person.scala
@@ -1,8 +1,8 @@
 package io.ryuichi
 
 sealed trait Sex
-object M extends Sex
-object F extends Sex
+case object M extends Sex
+case object F extends Sex
 
 case class Person(name: String, sex: Sex) {
   override def toString: String = name
