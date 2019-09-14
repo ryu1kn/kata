c8a367ca455a8e10dcb1fb9fe402e3002e404463
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2019-09-14 22:52:25 +1000

    FizzBuzz with scala

--- fizzbuzz/scala/src/main/scala/io/ryuichi/App.scala
+++ fizzbuzz/scala/src/main/scala/io/ryuichi/App.scala
@@ -1,5 +1,10 @@
 package io.ryuichi
 
 object App {
-  def transport(total: Int, ties: List[Int]): Int = 1
+  def fizzbuzz: Int => String = {
+    case n if n % 15 == 0 => "FizzBuzz"
+    case n if n % 5 == 0 => "Buzz"
+    case n if n % 3 == 0 => "Fizz"
+    case n => n.toString
+  }
 }
--- fizzbuzz/scala/src/test/scala/io/ryuichi/AppSpec.scala
+++ fizzbuzz/scala/src/test/scala/io/ryuichi/AppSpec.scala
@@ -5,8 +5,31 @@ import org.scalatest._
 
 class AppSpec extends WordSpec with Matchers {
 
-  "transport 1 person" in {
-    transport(1, List()) shouldEqual 1
+  "returns the same number" in {
+    fizzbuzz(1) shouldEqual "1"
   }
 
+  "returns the same number - 2" in {
+    fizzbuzz(2) shouldEqual "2"
+  }
+
+  "returns Fizz if it's 3" in {
+    fizzbuzz(3) shouldEqual "Fizz"
+  }
+
+  "returns Fizz if it's divisible by 3" in {
+    fizzbuzz(6) shouldEqual "Fizz"
+  }
+
+  "returns Buzz if it's 5" in {
+    fizzbuzz(5) shouldEqual "Buzz"
+  }
+
+  "returns Buzz if it's divisible by 5" in {
+    fizzbuzz(10) shouldEqual "Buzz"
+  }
+
+  "returns FizzBuzz if it's 15" in {
+    fizzbuzz(15) shouldEqual "FizzBuzz"
+  }
 }
