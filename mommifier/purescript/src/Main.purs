069b79a44195174fdbbb54bac0dfb57d0fd85232
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-04-25 22:01:13 +1000

    Did some refactoring

--- mommifier/purescript/src/Main.purs
+++ mommifier/purescript/src/Main.purs
@@ -2,33 +2,30 @@ module Main where
 
 import Prelude
 
-import Data.Array (elem, filter, length) as A
-import Data.String (Pattern(..), split, joinWith, length)
+import Data.Array (elem, filter, length, concatMap)
 import Data.Int (toNumber)
+import Data.String.CodeUnits (fromCharArray, toCharArray)
 
 mommify :: String -> String
-mommify = applyIf shouldMommify (joinWith "" <<< map mommify' <<< chars)
+mommify = fromCharArray <<< mommifyChars <<< toCharArray
+
+mommifyChars :: Array Char -> Array Char
+mommifyChars = applyIf shouldMommify (concatMap mommifyChar)
+
+mommifyChar :: Char -> Array Char
+mommifyChar s = if isVowel s then toCharArray "mommy" else [s]
+
+isVowel :: Char -> Boolean
+isVowel = flip elem (toCharArray "aeiou")
+
+shouldMommify :: Array Char -> Boolean
+shouldMommify = ((<) 0.3) <<< vowelRatio
+  where
+    vowelRatio = floatDiv <$> countVowels <*> length
+    countVowels = length <<< filter isVowel
 
 applyIf :: forall a. (a -> Boolean) -> (a -> a) -> a -> a
 applyIf p f x = if p x then f x else x
 
-mommify' :: String -> String
-mommify' s = if isVowel s then "mommy" else s
-
-isVowel :: String -> Boolean
-isVowel c = A.elem c ["a", "e", "i", "o", "u"]
-
-shouldMommify :: String -> Boolean
-shouldMommify = ((<) 0.3) <<< vowelRatio
-  where
-    vowelRatio :: String -> Number
-    vowelRatio = floatDiv <$> countVowels <*> length
-
-    countVowels :: String -> Int
-    countVowels = A.length <<< A.filter isVowel <<< chars
-
 floatDiv :: Int -> Int -> Number
 floatDiv x y = toNumber x / toNumber y
-
-chars :: String -> Array String
-chars = split (Pattern "")
