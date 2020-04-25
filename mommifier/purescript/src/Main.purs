09decff795b9a80eec354b7611c36f1920cc2adf
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-04-25 21:17:02 +1000

    PureScript version of mommifier

--- mommifier/purescript/src/Main.purs
+++ mommifier/purescript/src/Main.purs
@@ -2,9 +2,33 @@ module Main where
 
 import Prelude
 
-import Effect (Effect)
-import Effect.Console (log)
+import Data.Array (elem, filter, length) as A
+import Data.String (Pattern(..), split, joinWith, length)
+import Data.Int (toNumber)
 
-main :: Effect Unit
-main = do
-  log "🍝"
+mommify :: String -> String
+mommify = applyIf shouldMommify (joinWith "" <<< map mommify' <<< chars)
+
+applyIf :: forall a. (a -> Boolean) -> (a -> a) -> a -> a
+applyIf p f x = if p x then f x else x
+
+mommify' :: String -> String
+mommify' s = if isVowel s then "mommy" else s
+
+isVowel :: String -> Boolean
+isVowel c = A.elem c ["a", "e", "i", "o", "u"]
+
+shouldMommify :: String -> Boolean
+shouldMommify = ((<) 0.3) <<< vowelRatio
+  where
+    vowelRatio :: String -> Number
+    vowelRatio = floatDiv <$> countVowels <*> length
+
+    countVowels :: String -> Int
+    countVowels = A.length <<< A.filter isVowel <<< chars
+
+floatDiv :: Int -> Int -> Number
+floatDiv x y = toNumber x / toNumber y
+
+chars :: String -> Array String
+chars = split (Pattern "")
--- mommifier/purescript/test/Main.purs
+++ mommifier/purescript/test/Main.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Effect (Effect)
 import Effect.Aff (launchAff_)
+import Main (mommify)
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
@@ -11,6 +12,27 @@ import Test.Spec.Runner (runSpec)
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] do
-  describe "Math" do
-    it "equality" do
-      1 `shouldEqual` 1
+  describe "Mommifier" do
+    it "returns as is on given empty string" do
+      mommify "" `shouldEqual` ""
+
+    it "replace a vowel (a) to mommy" do
+      mommify "a" `shouldEqual` "mommy"
+
+    it "replace a vowel (i) to mommy" do
+      mommify "i" `shouldEqual` "mommy"
+
+    it "replace a vowel (e) to mommy" do
+      mommify "e" `shouldEqual` "mommy"
+
+    it "replace a vowel (o) to mommy" do
+      mommify "o" `shouldEqual` "mommy"
+
+    it "replace a vowel (u) to mommy" do
+      mommify "u" `shouldEqual` "mommy"
+
+    it "only replaces a vowel" do
+      mommify "ba" `shouldEqual` "bmommy"
+
+    it "returns as is if vowels are less than 30% of the string" do
+      mommify "bbba" `shouldEqual` "bbba"
