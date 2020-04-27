126b2ab9df20f9afcde9349b270f5734493019d4
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-04-27 15:49:00 +1000

    Up to one deduction step

--- coloured-triangles/purescript/src/Main.purs
+++ coloured-triangles/purescript/src/Main.purs
@@ -1,4 +1,25 @@
 module Main where
 
-findColour :: String -> String
-findColour x = x
+import Prelude
+
+import Data.List (List(..), (:), difference)
+import Data.List.NonEmpty (NonEmptyList(..), fromFoldable, toUnfoldable)
+import Data.Maybe (Maybe)
+import Data.NonEmpty ((:|))
+import Data.String.CodeUnits (fromCharArray, toCharArray)
+
+findColour :: String -> Maybe String
+findColour = (map <<< map) (fromCharList <<< findColour') toCharList
+  where
+    toCharList = fromFoldable <<< toCharArray
+    fromCharList = fromCharArray <<< toUnfoldable
+
+findColour' :: NonEmptyList Char -> NonEmptyList Char
+findColour' (NonEmptyList (x :| (y : _))) = NonEmptyList (nextColour x y :| Nil)
+findColour' (NonEmptyList (x :| _)) = NonEmptyList (x :| Nil)
+
+nextColour :: Char -> Char -> Char
+nextColour x y | x == y = x
+               | otherwise = case difference ('R' : 'G' : 'B' : Nil) (x : y : Nil) of
+                                (z : _) -> z
+                                Nil -> 'x'
--- coloured-triangles/purescript/test/Main.purs
+++ coloured-triangles/purescript/test/Main.purs
@@ -9,9 +9,22 @@ import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
+import Data.Maybe (Maybe(..))
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [consoleReporter] do
   describe "Coloured Triangles" do
-    it "single Blue" do
-      findColour "B" `shouldEqual` "B"
+    it "no characters" do
+      findColour "" `shouldEqual` Nothing
+
+    it "single red" do
+      findColour "R" `shouldEqual` Just "R"
+
+    it "two reds" do
+      findColour "RR" `shouldEqual` Just "R"
+
+    it "suppliment missing colour code" do
+      findColour "RB" `shouldEqual` Just "G"
+
+    it "two reduction steps" do
+      findColour "RRG" `shouldEqual` Just "G"
