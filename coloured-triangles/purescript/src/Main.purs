91a2bc3fa2687077315e44c3a688990ca545bba0
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-04-27 23:21:53 +1000

    Define new type for Colour

--- coloured-triangles/purescript/src/Main.purs
+++ coloured-triangles/purescript/src/Main.purs
@@ -2,25 +2,43 @@ module Main where
 
 import Prelude
 
-import Data.List (List(..), difference, fromFoldable, tail, zipWith, (:))
-import Data.Maybe (Maybe(..))
+import Data.List (List(..), difference, fromFoldable, tail, zipWith, (:), head)
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.String.CodeUnits (toCharArray)
+import Data.Traversable (sequence)
 
-findColour :: String -> Maybe Char
-findColour = deduces <<< toCharList
+data Colour = R | G | B
+
+derive instance eqColour :: Eq Colour
+
+instance showColour :: Show Colour where
+  show R = "R"
+  show G = "G"
+  show B = "B"
+
+toColour :: Char -> Maybe Colour
+toColour c | c == 'R' = Just R
+           | c == 'G' = Just G
+           | c == 'B' = Just B
+           | otherwise = Nothing
+
+findColour :: String -> Maybe String
+findColour s = do
+    colours <- toColourList s
+    colour <- deduces colours
+    pure $ show colour
   where
-    toCharList = fromFoldable <<< toCharArray
+    toColourList :: String -> Maybe (List Colour)
+    toColourList = sequence <<< map toColour <<< fromFoldable <<< toCharArray
 
-deduces :: List Char -> Maybe Char
+deduces :: List Colour -> Maybe Colour
 deduces (x : Nil) = Just x
 deduces list = deduce list >>= deduces
 
-deduce :: List Char -> Maybe (List Char)
+deduce :: List Colour -> Maybe (List Colour)
 deduce (x : Nil) = Just (x : Nil)
 deduce list = map (zipWith nextColour list) (tail list)
 
-nextColour :: Char -> Char -> Char
+nextColour :: Colour -> Colour -> Colour
 nextColour x y | x == y = x
-               | otherwise = case difference ('R' : 'G' : 'B' : Nil) (x : y : Nil) of
-                                  (z : _) -> z
-                                  Nil -> 'x'
+               | otherwise = fromMaybe R $ head $ difference (R : G : B : Nil) (x : y : Nil)
