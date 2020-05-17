c310c167a6079882eb93fe9ba712484719bfd368
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-05-17 18:40:53 +1000

    [purs,roman-numeral] Simplify cases by introducing = 0 case

--- roman-numeral/purescript/src/Main.purs
+++ roman-numeral/purescript/src/Main.purs
@@ -6,12 +6,11 @@ import Data.List.Lazy (List, repeat, take, toUnfoldable)
 import Data.String.CodeUnits (fromCharArray)
 
 romanNumeral :: Int -> String
-romanNumeral x | x < 4 = toString <<< flip take (repeat 'I') $ x
+romanNumeral x | x == 0 = ""
+               | x < 4 = toString <<< flip take (repeat 'I') $ x
                | x == 4 = "IV"
-               | x == 5 = "V"
                | x < 9 = "V" <> romanNumeral (x - 5)
                | x == 9 = "IX"
-               | x == 10 = "X"
                | otherwise = "X" <> romanNumeral (x - 10)
 
 toString :: List Char -> String
