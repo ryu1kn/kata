7eb16a56199597a4b35a91d046b58a86a2daf502
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-05-17 18:53:09 +1000

    [purs,roman-numeral] Subtracting is -1 from the base number

--- roman-numeral/purescript/src/Main.purs
+++ roman-numeral/purescript/src/Main.purs
@@ -7,10 +7,11 @@ import Data.String.CodeUnits (fromCharArray)
 
 romanNumeral :: Int -> String
 romanNumeral x | x < 4 = toString <<< flip take (repeat 'I') $ x
-               | x == 4 = "IV"
-               | x < 9 = "V" <> romanNumeral (x - 5)
-               | x == 9 = "IX"
-               | otherwise = "X" <> romanNumeral (x - 10)
+               | x < 9 = subtractSymbol 5 x <> "V" <> romanNumeral (x - 5)
+               | otherwise = subtractSymbol 10 x <> "X" <> romanNumeral (x - 10)
+
+subtractSymbol :: Int -> Int -> String
+subtractSymbol base x = if x < base then "I" else ""
 
 toString :: List Char -> String
 toString = toUnfoldable >>> fromCharArray
