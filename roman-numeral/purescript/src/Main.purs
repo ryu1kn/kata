e317f10889b4aee5e9128a30a0896de8d0314c06
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-05-17 21:25:44 +1000

    [purs,roman-numeral] Roman number and its value should be related

--- roman-numeral/purescript/src/Main.purs
+++ roman-numeral/purescript/src/Main.purs
@@ -5,13 +5,18 @@ import Prelude
 import Data.List.Lazy (List, repeat, take, toUnfoldable)
 import Data.String.CodeUnits (fromCharArray)
 
+type RN = { n :: Int, c :: String }
+
+_V = {n: 5, c: "V"}
+_X = {n: 10, c: "X"}
+
 romanNumeral :: Int -> String
 romanNumeral x | x < 4 = toString <<< flip take (repeat 'I') $ x
-               | x < 9 = symbolsAround 5 "V" x
-               | otherwise = symbolsAround 10 "X" x
+               | x < 9 = symbolsAround _V x
+               | otherwise = symbolsAround _X x
 
-symbolsAround :: Int -> String -> Int -> String
-symbolsAround b bc x = subtractSymbol b x <> bc <> romanNumeral (x - b)
+symbolsAround :: RN -> Int -> String
+symbolsAround rn x = subtractSymbol rn.n x <> rn.c <> romanNumeral (x - rn.n)
   where
     subtractSymbol base n = if n < base then "I" else ""
 
