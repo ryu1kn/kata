3f6ff42f254c82fe5226b26736d0df05979e4098
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-05-08 23:57:10 +1000

    [mommifier] Mommify per char

--- mommifier/haskell/src/Lib.hs
+++ mommifier/haskell/src/Lib.hs
@@ -3,4 +3,10 @@ module Lib
     ) where
 
 mommify :: String -> String
-mommify s = s
+mommify s = concat $ map mommifyChar s
+
+mommifyChar :: Char -> String
+mommifyChar c = if isVowel c then "mommy" else (c : "")
+
+isVowel :: Char -> Bool
+isVowel = flip elem $ "aeiou"
--- mommifier/haskell/test/Spec.hs
+++ mommifier/haskell/test/Spec.hs
@@ -9,3 +9,21 @@ spec = describe "mommifier" $ do
 
     it "returns an empty string as is" $
         mommify "" `shouldBe` ""
+
+    it "returns 'mommy' if 'a' is given" $
+        mommify "a" `shouldBe` "mommy"
+
+    it "returns 'mommy' if 'e' is given" $
+        mommify "e" `shouldBe` "mommy"
+
+    it "returns 'mommy' if 'i' is given" $
+        mommify "i" `shouldBe` "mommy"
+
+    it "returns 'mommy' if 'o' is given" $
+        mommify "o" `shouldBe` "mommy"
+
+    it "returns 'mommy' if 'u' is given" $
+        mommify "u" `shouldBe` "mommy"
+
+    it "replaces to mommy only for a vowel" $
+        mommify "ba" `shouldBe` "bmommy"
