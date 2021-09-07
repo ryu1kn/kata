28ed3aa934994b52d1b643e16d2b3f485858ff97
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2021-09-07 23:15:21 +1000

    [mommifier] Extract the part that reduce vowels

--- mommifier/kotlin/app/src/main/kotlin/mommifier/kotlin/Mommifier.kt
+++ mommifier/kotlin/app/src/main/kotlin/mommifier/kotlin/Mommifier.kt
@@ -2,7 +2,9 @@ package mommifier.kotlin
 
 object Mommifier {
     fun mommify(s: String): String =
-        (if (s == "aa" || s == "ae") "a" else s).map(::mommifyCharacter).joinToString("")
+        squashVowels(s).map(::mommifyCharacter).joinToString("")
+
+    private fun squashVowels(s: String) = (if (s == "aa" || s == "ae") "a" else s)
 
     private fun mommifyCharacter(c: Char) = if (isVowel(c)) "mommy" else c.toString()
 
